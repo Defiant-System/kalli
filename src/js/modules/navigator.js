67708977bc8086e2455c83c36f45cf15f46fcc43
Hakan Bilgin <hbi99@hotmail.com>
2023-05-26 14:06:41 +0200

    more UI

--- src/js/modules/navigator.js
+++ src/js/modules/navigator.js
@@ -6,7 +6,7 @@
 		// fast references
 		this.doc = $(document);
 		this.els = {
-			zoomRect: window.find(".area.navigator .body > div"),
+			zoomRect: window.find(".area.navigator .body .view-rect"),
 		};
 		
 		// bind event handlers
@@ -29,10 +29,42 @@
 		// console.log(event);
 		switch (event.type) {
 			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+				// prepare drag object
+				let el = $(event.target);
+				// drag object
+				Self.drag = {
+					el,
+					// file: Proj.file,
+					clickX: +el.prop("offsetLeft") - event.clientX,
+					clickY: +el.prop("offsetTop") - event.clientY,
+					min: { x: 0, y: 0 },
+					max: {
+						x: +el.parent().prop("offsetWidth") - +el.prop("offsetWidth"),
+						y: +el.parent().prop("offsetHeight") - +el.prop("offsetHeight"),
+						// w: Proj.aW - Proj.file.width,
+						// h: Proj.aH - Proj.file.height,
+					},
+					_max: Math.max,
+					_min: Math.min,
+				};
+				// prevent mouse from triggering mouseover
+				APP.els.content.addClass("cover");
+				// bind event handlers
+				Self.doc.on("mousemove mouseup", Self.pan);
 				break;
 			case "mousemove":
+				let left = Drag._min(Drag._max(event.clientX + Drag.clickX, Drag.min.x), Drag.max.x),
+					top = Drag._min(Drag._max(event.clientY + Drag.clickY, Drag.min.y), Drag.max.y);
+				// moves navigator view rectangle
+				Drag.el.css({ top, left });
 				break;
 			case "mouseup":
+				// remove class
+				APP.els.content.removeClass("cover");
+				// unbind event handlers
+				Self.doc.off("mousemove mouseup", Self.pan);
 				break;
 		}
 	}
